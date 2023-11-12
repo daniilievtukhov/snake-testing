1646196afff2f065125b51c099d9c18c3d7a81ca
Danya <daniilka003@gmail.com>
2023-11-12 18:42:26 +0200

    deploy back

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    user: "postgres",
+    password: "Daniil200301",
+    port: 5432,
+    database: "node_postgres",
 });
 
 module.exports = pool;
