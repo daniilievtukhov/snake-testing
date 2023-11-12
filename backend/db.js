9d832a0a4fec72817233adbc35b20895428ac4b5
Danya <daniilka003@gmail.com>
2023-11-12 19:15:02 +0200

    deploy

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    //connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-    user: "postgres",
-    password: "Daniil200301",
-    port: 5432,
-    database: "node_postgres",
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    // user: "postgres",
+    // password: "Daniil200301",
+    // port: 5432,
+    // database: "node_postgres",
     //host: "localhost",
 });
 
--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
+app.listen(process.env.PORT, () =>
+    console.log(`Server started on port ${PORT} `)
+);
